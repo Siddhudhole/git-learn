f7a0e4eca1b712ff6308e0660bb2f486e6498906
Siddharth Dhole <shidhudhole358@gmail.com>
2024-09-22 15:01:27 +0530

    update app file

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st 
 
 st.set_page_config(page_title='chat with me ')
+st.write(
+    """
+    Welcome to the chatbot interface! Type your message below to interact with me.
+    """
+)
+
+user_message = st.text_input('Your message')
+
+if user_message:
+    response = 'Chatbot:'+ user_message
+    st.write(response
+"""
+)
